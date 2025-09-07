ac7334a949a3ed0d09fe8659c1510b8fabb4bec6
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-07 11:17:55 +0530

    created healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,10 +1,17 @@
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+/**
+ * * This controller is used to verify if the server is up and running. 
+ */
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+
+    return res
+        .status(200)
+    .json(new ApiResponse(200,{status:"ok"},"server is healthy"))
+
 })
 
 export {

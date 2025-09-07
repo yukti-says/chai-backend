07d84f67a693f68779533f3b0a1c80d1b389d8d4
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-07 07:17:50 +0530

    comment controller and tweet controller done.

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,26 +1,159 @@
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+/**
+ * ===========================
+ * GET ALL COMMENTS FOR A VIDEO
+ * ===========================
+ */
+
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const {videoId} = req.params //* from url
+    const { page = 1, limit = 10 } = req.query //*extracted pagination values from query
+
+    //* validating if videoId is a valid MongoDb ObjectId
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400,"Invalid videoId")
+    }
+
+    //* aggregation plugin to fetch comments with owner details
+    const comments = await Comment.aggregatePaginate(
+        Comment.aggregate([
+            //* match the comments that is belonging to that video
+            {
+                $match: {
+                    video: new mongoose.Types.ObjectId(videoId)
+                }
+            },
+            //* sort the comments by latest first
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            },
+            //* lookup find throw joining users to get owner details
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner"
+                }
+            },
+            //* convert the owner array to abject
+            {
+                $unwind: "$owner"
+            },
+            //* now project-> return only selected values
+            {
+                $project: {
+                    content: 1,
+                    createdAt: 1,
+                    "owner.username": 1,
+                    "owner.avatar": 1,
+                    "owner.fullname": 1,
+                }
+            }
+        ]),
+        { page, limit }
+    );
+
+    return res.json(new ApiResponse(200,comments,"comments fetched successfully"))
+    
 
 })
+
+
+/**
+ * ===========================
+ * ADD A COMMENT TO A VIDEO
+ * ===========================
+ */
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    //* getting the video id and content
+    const { videoId } = req.params;
+    const { content } = req.body;
+
+    //* validate input
+    if (!content) {
+        throw new ApiError(400,"Comment content is required")
+    }
+
+    //*  validate the videoId
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400,"Invalid Video ID")
+    }
+
+    //* creating a new comment document
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner:req.user._id //? only logged in user can create the comment 
+    })
+
+    return res.json(new ApiResponse(201,comment,"comment Added!"))
 })
+
+/**
+ * ===========================
+ * UPDATE A COMMENT
+ * ===========================
+ */
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
 
+  //* Find the comment by ID
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  //* Ensuring  only the comment owner can update it
+  if (comment.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You can only update your own comments");
+  }
+
+  //* Update comment content
+  comment.content = content || comment.content;
+    await comment.save();
+    
+
+    return res.json(new ApiResponse(200, comment, "Comment updated successfully"));
+});
+
+/**
+ * ===========================
+ * DELETE A COMMENT
+ * ===========================
+ */
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const { commentId } = req.params;
+    
+    //* find the comment in the db
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw new ApiError(404,"comment not found")
+    }
+
+    //* ensuring only owner can delete its comments
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403,"You can only delete your own comments")
+    }
+
+    await comment.deleteOne();
+
+    return res.json(new ApiResponse(200, {}, "Comment deleted successfully"));
 })
 
 export {
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,25 +5,122 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+
+/**
+ * ===========================
+ * CREATE A TWEET
+ * ===========================
+ */
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    //* get the content from req.body
+    const { content } = req.body;
+    if (!content) {
+        throw new ApiError(400, "content is required");
+    }
+    //* now creating a tweet document with provided content and logged in user as owner
+    const tweet = await Tweet.create({
+        content,
+        owner:req.user._id,
+    })
+
+    //* send response with the newly created tweet
+    return res
+    .json(new ApiResponse(201,tweet,"Tweet created!"))
 })
 
+/**
+ * ===========================
+ * GET ALL TWEETS
+ * ===========================
+ */
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    //* fetching all the tweets
+    const tweets = await Tweet.find().populate("owner", "username avatar fullname") //* populated owner field with user details
+        .sort({ createdAt: -1 }); //* sorted tweets latest first
+    
+    return res.json(new ApiResponse(200,tweets,"all tweets fetched!"))
 })
+
+/**
+ * ===========================
+ * GET SINGLE TWEET BY ID
+ * ===========================
+ */
+const singleTweetById = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+    const { tweetId } = req.params;
+
+    //* find tweets by id and fetch its owner details
+    const tweet = await Tweet.findById(tweetId).populate("owner", "username avatar fullname");
+
+    if (!tweet) {
+        throw new ApiError(404,"Tweet not found")
+    }
+
+    return res.json(new ApiResponse(200,tweet,"Tweet fetched"))
+})
+
+/**
+ * ===========================
+ * UPDATE A TWEET
+ * ===========================
+ */
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    //* get tweet if from url and content from req.body
+    const { tweetId } = req.params;
+    const { content } = req.body;
+
+    //* find tweet in db
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404,"Tweet not found")
+    }
+
+    //* ensuring only the owner can update the tweet
+    if (tweet.owner.toString() != req.user._id.toString()) {
+        throw new ApiError(403,"You can only update your own tweets")
+    }
+
+    //* now update the tweets if new content is given
+    tweet.content = content || tweet.content;
+    await tweet.save() //? save changes
+
+    return res.json(new ApiResponse(200,tweet,"Tweet Updated!"))
 })
 
+/**
+ * ===========================
+ * DELETE A TWEET
+ * ===========================
+ */
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    const { tweetId } = req.params;
+    //* find tweet in db
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404,"Tweet not found")
+    }
+
+    //* ensuring only owner can delete its tweets
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403,"you can only delete your own tweets")
+    }
+
+    //* delete the tweet
+    await tweet.deleteOne();
+
+    return res.json(new ApiResponse(200,{},"Tweet deleted successfully"))
 })
 
 export {
     createTweet,
     getUserTweets,
     updateTweet,
-    deleteTweet
+    deleteTweet,
+    singleTweetById
 }
